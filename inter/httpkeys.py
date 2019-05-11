883895935958fc00aab107d5771107822a98f9f2
Will <1052949192@qq.com>
2019-05-11 22:23:13 +0800

    更新反射调用和数据驱动
    Signed-off-by: Will <1052949192@qq.com>

--- inter/httpkeys.py
+++ inter/httpkeys.py
@@ -9,7 +9,7 @@ class HTTP():
     提供接口测试结果断言的各种方法
     """
 
-    def __init__(self):
+    def __init__(self, w):
         # 在创建对象的时候初始化session
         self.session = requests.session()
         # 设置请求的基本url
@@ -18,10 +18,13 @@ class HTTP():
         self.result = None
         self.jsonres = None
         self.json = {}
+        self.writer = w
 
     def seturl(self, u):
         # 设置请求的基本url
         self.url = u + "/"
+        self.writer.write(self.writer.row,self.writer.clo,"PASS")
+        self.writer.write(self.writer.row, self.writer.clo+1, str(self.url))
 
     def removeheader(self, key):
         # 从请求的头里面删除key这个键
@@ -30,31 +33,54 @@ class HTTP():
         except Exception as e:
             print(e)
 
-    def addheader(self,key,value):
+        self.writer.write(self.writer.row, self.writer.clo, "PASS")
+        self.writer.write(self.writer.row, self.writer.clo + 1, str(self.session.headers))
+
+    def addheader(self, key, value):
         # 往请求的头里面添加一个键值对
         value = self.__get_value(value)
         self.session.headers[key] = value
+        self.writer.write(self.writer.row, self.writer.clo, "PASS")
+        self.writer.write(self.writer.row, self.writer.clo + 1, str(self.session.headers))
 
     def post(self, path, params=None):
         # 解析参数为一个dict
         self.__get_params(params)
         # 调用post，请求接口
         self.result = self.session.post(self.url + path, data=self.params)
-        self.jsonres = json.loads(self.result.text)
+        print(self.result.text)
+        self.jsonres = json.loads(self.__to_json(self.result.text))
+        self.writer.write(self.writer.row, self.writer.clo, "PASS")
+        self.writer.write(self.writer.row, self.writer.clo + 1, str(self.jsonres))
+
+    def get(self, path, params=None):
+        # 调用post，请求接口
+        self.result = self.session.get(self.url + path + '?' + params)
+        self.jsonres = json.loads(self.__to_json(self.result.text))
+        self.writer.write(self.writer.row, self.writer.clo, "PASS")
+        self.writer.write(self.writer.row, self.writer.clo + 1, str(self.jsonres))
+
+    def __to_json(self, res):
+        return res[res.find('{'):res.rfind('}') + 1]
 
     def __get_params(self, p):
         # 每一次解析参数之前先清空参数dict
         self.params.clear()
 
-        if p is None:
+        if p is None or p == '':
             return self.params
 
-        # 分割每一个参数
+        # 分割每一个键值对
         pp = p.split('&')
+        # 遍历键值对
         for s in pp:
             # 分割每一个参数的键和值
             ppp = s.split('=')
-            self.params[ppp[0]] = ppp[1]
+            # print(ppp)
+            try:
+                self.params[ppp[0]] = ppp[1]
+            except Exception as e:
+                self.params[ppp[0]] = None
 
         return self.params
 
@@ -67,21 +93,27 @@ class HTTP():
         """
         if str(self.jsonres[key]) == value:
             print("PASS")
+            self.writer.write(self.writer.row, self.writer.clo, "PASS")
+            self.writer.write(self.writer.row, self.writer.clo + 1, self.jsonres[key])
         else:
             print("FAIL")
+            self.writer.write(self.writer.row, self.writer.clo, "FAIL")
+            self.writer.write(self.writer.row, self.writer.clo + 1, self.jsonres[key])
 
-    def savejson(self,key,p):
+    def savejson(self, key, p):
         """
         将返回结果里面，json的key的值，保存到我们框架的p这个参数里面
         :param key: 需要保存的json值的键
         :param p: 保存后的参数名
         :return: 无
         """
+        print(self.jsonres)
         self.json[p] = self.jsonres[key]
+        self.writer.write(self.writer.row, self.writer.clo, "PASS")
+        self.writer.write(self.writer.row, self.writer.clo + 1, str(self.json))
 
-    def __get_value(self,p):
+    def __get_value(self, p):
         # 用{key}的形式去替换参数里面的字符串，如没有就不会替换
         for key in self.json.keys():
             p = p.replace('{' + key + '}', self.json[key])
         return p
-
--- mainRunner.py
+++ mainRunner.py
@@ -1,8 +1,63 @@
 # coding:utf8
+from common.Excel import Reader, Writer
+from inter.httpkeys import HTTP
+import inspect
+
+
+def runCases(http, line):
+    """
+    执行每一行用例的方法
+    :param line: 参数列表
+    :return: 无
+    """
+    if len(line[0]) > 0 or len(line[1]) > 0:
+        # 分组信息不用执行
+        pass
+    else:
+        func = getattr(http, line[3])
+        p = inspect.getfullargspec(func).__str__()
+        p = p[p.find('args=') + 5:p.find(', varargs')]
+        p = eval(p)
+        p.remove('self')
+
+        if len(p) == 0:
+            func()
+        elif len(p) == 1:
+            func(line[4])
+        elif len(p) == 2:
+            func(line[4], line[5])
+        elif len(p) == 3:
+            func(line[4], line[5], line[6])
+        else:
+            print("warning：框架暂时只支持3个参数！")
 
 
 if __name__ == "__main__":
     print("整个框架使用该入口执行")
+    reader = Reader()
+    reader.open_excel('./lib/HTTP接口用例.xls')
+    sheetname = reader.get_sheets()
+    print(sheetname)
 
+    writer = Writer()
+    writer.copy_open('./lib/HTTP接口用例.xls', './lib/result-HTTP接口用例.xls')
 
+    http = HTTP(writer)
 
+    for sheet in sheetname:
+        # 设置当前读取的sheet页面
+        reader.set_sheet(sheet)
+        writer.set_sheet(sheet)
+        writer.clo = 7
+
+        for i in range(reader.rows):
+            writer.row = i
+            line = reader.readline()
+            # 执行用例的方法
+            print(line)
+            try:
+                runCases(http, line)
+            except Exception as e:
+                pass
+
+    writer.save_close()
